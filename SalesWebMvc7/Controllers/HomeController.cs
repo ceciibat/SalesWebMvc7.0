c216c97284479ecef47d6f9a4335d853e49a9a71
Ana Cecília <anaceciliabatalha@gmail.com>
2023-08-31 16:14:48 -0400

    Primeiro controller e testes nas páginas Razor

--- SalesWebMvc7/Controllers/HomeController.cs
+++ SalesWebMvc7/Controllers/HomeController.cs
@@ -18,6 +18,13 @@ namespace SalesWebMvc7.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Professor"] = "Nelio Alves";
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
