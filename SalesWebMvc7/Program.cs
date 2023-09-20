4a0b1c23e5bfeda5061659fc8ce4f40326b4ab46
Ana Cecília <anaceciliabatalha@gmail.com>
2023-09-20 15:13:26 -0400

    Localização e formatação de números e datas

--- SalesWebMvc7/Program.cs
+++ SalesWebMvc7/Program.cs
@@ -1,18 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Localization;
 using SalesWebMvc7.Data;
 using SalesWebMvc7.Services;
+using System.Globalization;
 using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var conn = builder.Configuration.GetConnectionString("SalesWebMvc7Context");
 //variavel auxiliar
-
-
-//builder.Services.AddDbContext<SalesWebMvc7Context>(options =>
-//    options.UseMySql((conn), builder =>
-//builder.MigrationsAssembly("SalesWebMvc7")));
 
 builder.Services.AddDbContext<SalesWebMvc7Context>(options =>
 {
@@ -31,6 +28,16 @@ var app = builder.Build();
 
 app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
 
+// DEFININDO A LOCALIZAÇÃO
+var enUS = new CultureInfo("en-US");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(enUS),
+    SupportedCultures = new List<CultureInfo> { enUS },
+    SupportedUICultures = new List<CultureInfo> { enUS }
+};
+
+app.UseRequestLocalization(localizationOptions);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
